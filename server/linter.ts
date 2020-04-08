5b57c0afb0766309361b121c99f35ed3614c1275
Travis Illig <tillig@paraesthesia.com>
2020-04-08 13:09:07 -0700

    Resolve #6: Pre-decode file URIs since Spectral doesn't do that for us.

--- server/linter.ts
+++ server/linter.ts
@@ -7,6 +7,7 @@ import { parseYaml } from '@stoplight/spectral/dist/parsers';
 import { IParsedResult } from '@stoplight/spectral/dist/document';
 import { IRuleset } from '@stoplight/spectral/dist/types/ruleset';
 import { getLocationForJsonPath } from '@stoplight/yaml';
+import { URI } from 'vscode-uri';
 import { refResolver } from './resolver';
 import { registerFormats } from './formats';
 
@@ -48,12 +49,23 @@ export class Linter {
     // It's unclear why JSON and YAML both get parsed as YAML, but that's how Spectral does it, sooooooo...
     const text = document.getText();
     const spec = parseYaml(text);
+
+    // There's a bug in how `json-ref-resolver` handles file:// URLs - it fails
+    // to decode any URL-encoded items because it treats a file://path/to/file
+    // and /fs/path/to/file style URL as the same thing. To bypass the issue we
+    // pre-convert the file URL into a local FS path.
+    //
+    // For in-memory documents, this URI will be `untitled:Untitled-1` or
+    // something similar. The `fsPath` will be `Untitled-1`, and that seems as
+    // reasonable as anything else for in-memory validation. This only really
+    // matters when parsing the relative links in references anyway.
+    const file = URI.parse(document.uri).fsPath;
     const parsedResult: IParsedResult = {
-      source: document.uri,
+      source: file,
       parsed: spec,
       getLocationForJsonPath,
     };
 
-    return this.spectral.run(parsedResult, { ignoreUnknownFormat: true, resolve: { documentUri: document.uri } });
+    return this.spectral.run(parsedResult, { ignoreUnknownFormat: true, resolve: { documentUri: file } });
   }
 }
